904425f5342e9ae8cc7a1bb2eb5fe82cbc77b122
Ubuntu <ubuntu@demo-dspace.sara-service.org>
2018-07-10 06:50:34 +0000

    adding joerg for later tests

--- dspace-init.sh
+++ dspace-init.sh
@@ -14,6 +14,7 @@ sudo /dspace/bin/dspace user --add --email matthias.fratz@uni-konstanz.de --pass
 sudo /dspace/bin/dspace user --add --email daniel.scharon@uni-konstanz.de --password SaraTest --givenname Daniel --surname Scharon
 sudo /dspace/bin/dspace user --add --email kosmas.kaifel@uni-ulm.de --password SaraTest --givenname Kosmas --surname Kaifel
 sudo /dspace/bin/dspace user --add --email uli.hahn@uni-ulm.de --password SaraTest --givenname Uli --surname Hahn
+sudo /dspace/bin/dspace user --add --email joerg.domaschka@uni-ulm.de --password SaraTest --givenname Jörg --surname Domaschka
 # generic SARA test user
 sudo /dspace/bin/dspace user --add --email demo-user@sara-service.org --password SaraTest --givenname Demo --surname User
 # generic SARA test user, no submit rights
